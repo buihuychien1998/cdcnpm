fdad4e5e98558a805bc28c923a93a58f9c042c47
buihuychien1998 <35371090+buihuychien1998@users.noreply.github.com>
2019-10-03 21:16:35 +0700

    init file

---
+++ home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" href="homestyle.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+	<div class="menu-bar">
+		<ul>
+			<li class="active"><a href="home.php"><i class="fa fa-home"></i>Home</a></li>
+			<li><a href="product.php"><i class="fa fa-product-hunt"></i>Product</a></li>
+			<li><a href="accessories.php"><i class="fa fa-clone"></i>Accessories</a>
+			<div class="sub-menu-1">
+					<ul>
+						<li><a href="">Mouse</a></li>
+						<li><a href="">Screen</a></li>
+						<li><a href="">KeyBoard</a></li>
+						<li><a href="">RAM</a></li>
+					</ul>
+				</div></li>
+			<li><a href="about.php"><i class="fa fa-user"></i>About Us</a>
+				<div class="sub-menu-1">
+					<ul>
+						<li><a href="">Mission</a></li>
+						<li><a href="">Vision</a></li>
+						<li><a href="">Team</a></li>
+					</ul>
+				</div>
+			</li>
+			<li><a href="login.php"><i class="fa fa-sign-in"></i>Login</a></li>
+		</ul>
+	</div>
+</body>
+</html>

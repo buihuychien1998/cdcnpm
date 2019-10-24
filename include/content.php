ec12638ce694b8187913388d2af8b6182551e600
buihuychien1998 <35371090+buihuychien1998@users.noreply.github.com>
2019-10-24 22:19:13 +0700

    partition layout

---
+++ include/content.php
@@ -0,0 +1,116 @@
+ <main>
+ 	<div class="col-md-12 slider">
+ 			<div id="myCarouse" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#myCarouse" data-slide-to="0" class="active"></li>
+				    <li data-target="#myCarouse" data-slide-to="1"></li>
+				    <li data-target="#myCarouse" data-slide-to="2"></li>
+				    <li data-target="#myCarouse" data-slide-to="3"></li>
+				  </ol>
+
+				  <!-- Wrapper for slides -->
+				   <div class="carousel-inner">
+					    <div class="carousel-item active mySlide" data-interval="10000">
+					      <img src="images/img_forest.jpg" class="d-block w-100">
+					    </div>
+					    <div class="carousel-item mySlide" data-interval="2000">
+					      <img src="images/img_lights.jpg" class="d-block w-100">
+					    </div>
+					    <div class="carousel-item mySlide" data-interval="2000">
+					      <img src="images/img_mountains.jpg" class="d-block w-100">
+					    </div>
+					    <div class="carousel-item mySlide" data-interval="2000">
+					      <img src="images/img_snowtops.jpg" class="d-block w-100">
+					    </div>
+				  </div>
+						  
+			</div>
+			
+ 	</div>
+	<div class="row content">
+	<div class="col-md-3 nav">
+		<div class="nav-bar">
+			<ul class="">
+		    	<li class="nav-item">
+		    		<a class="" href="#"><i class="fa fa-clone"></i>Macbook Pro Retina</a>
+		    	</li>
+		    	<li class="nav-item">
+		    		<a class="" href="#"><i class="fa fa-home"></i>Macbook Air</a>
+		   		 </li>
+			    <li class="nav-item">
+			    	<a class="" href="#"><i class="fa fa-home"></i>Macbook 12 inch</a>
+			    </li>
+			    <li class="nav-item">
+			    	<a class="" href="#" ><i class="fa fa-phone"></i>Macbook Pro Touch</a>
+			    </li>
+			    <li class="nav-item">
+			    	<a class="" href="#" ><i class="fa fa-phone"></i>Macbook Pro Touch</a>
+			    </li>
+			    <li class="nav-item">
+			    	<a class="" href="#" ><i class="fa fa-phone"></i>Macbook Pro Touch</a>
+			    </li>
+			    <li class="nav-item">
+			    	<a class="" href="#" ><i class="fa fa-phone"></i>Macbook Pro Touch</a>
+			    </li>
+			</ul>
+		</div>
+	</div>
+		<div class="col-md-8 product_row">
+			<div class="product_box">
+	        <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_01.jpg" alt="image 1" /></a></div>
+	        <h3>Name Laptop</h3>
+	        <p>Price</p>
+		</div>
+		<div class="product_box">
+	    <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_02.jpg" alt="image 2" /></a></div>
+	    <h3>Name Laptop</h3>
+	    <p>Price</p>
+		</div>
+
+	    <div class="product_box">
+	        <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_03.jpg" alt="image 3" /></a></div>
+	        <h3>Name Laptop</h3>
+	        <p>Price</p>
+	    </div>
+	    
+		<div class="product_box">
+	        <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_04.jpg" alt="image 4" /></a></div>
+	        <h3>Name Laptop</h3>
+	        <p>Price</p>
+	    </div>
+	    
+		<div class="cleaner"></div>
+	    
+	  	<div class="product_box">
+	        <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_05.jpg" alt="image 5" /></a></div>
+	        <h3>Name Laptop</h3>
+	        <p>Price</p>
+	    </div>
+	    
+	    <div class="product_box">
+		    <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_06.jpg" alt="image 6" /></a></div>
+	        <h3>Name Laptop</h3>
+	    	<p>Price</p>
+	    </div>
+	    
+	    <div class="product_box">
+	         <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_07.jpg" alt="image 7" /></a></div>
+	         <h3>Name Laptop</h3>
+		     <p>Price</p>
+	    </div>
+	    
+		<div class="product_box">
+	         <div class="thumb_wrapper"><a href="detailproduct.php"><img src="images/templatemo_image_08.jpg" alt="image 8" /></a></div>
+	         <h3>Name Laptop</h3>
+		     <p>Price</p>
+	    </div>
+	    <div class="partition_page">
+	    	<?php
+		   		
+		    ?>
+	    </div>
+		 
+	 	</div>
+	</div>
+ </main>
